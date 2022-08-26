5016af2469e7db7d79ff5fc2cc4007a798181d8a
AlexBash82 <kometa.4m@gmail.com>
2022-08-26 23:40:11 +0500

    refStep

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import Gamefield from './Component/Gamefield'
 
 function App() {
-  let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16, 15]
-  return <Gamefield arr={arr} />
+  return <Gamefield />
 }
 
 export default App
--- src/Component/Gamefield.js
+++ src/Component/Gamefield.js
@@ -1,26 +1,53 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import './Gamefield.scss'
 import Square from './Square'
 
-function Gamefield({ arr }) {
-  const [fifteen, setFifteen] = useState(arr)
+function Gamefield() {
+  const [fifteen, setFifteen] = useState([])
   const [finish, setFinish] = useState(false)
+  const [steps, setSteps] = useState(0)
+  const refArrow = useRef([])
 
   function drug(order, iSpace) {
-    let fifteenArr = fifteen
+    let fifteenArr = fifteen.slice()
     let itemA = fifteenArr[order]
     let itemB = fifteenArr[iSpace]
     fifteenArr[order] = itemB
     fifteenArr[iSpace] = itemA
-    setFifteen(fifteenArr.concat([]))
+    setFifteen(fifteenArr)
+    setSteps((prevSteps) => prevSteps + 1)
     let result = fifteenArr.filter((item, index) => --item === index)
     if (result.length === 16) {
       setFinish(true)
     }
   }
 
+  function start() {
+    let arrow = []
+    let random = () => Math.floor(1 + Math.random() * 16)
+    arrow.push(random())
+    while (arrow.length < 16) {
+      let num = random()
+      let result = arrow.find((item) => item === num)
+      if (!result) {
+        arrow.push(num)
+      }
+    }
+    refArrow.current = arrow
+    setFifteen(arrow)
+    setSteps(0)
+  }
+
+  function restart() {
+    setFifteen(refArrow.current)
+    setSteps(0)
+  }
+
   return (
     <div className="Screen">
+      <button onClick={() => start()}>Start</button>
+      <button onClick={() => restart()}>Restart</button>
+      <div>Amount steps: {steps}</div>
       <div className="Screen_field">
         {fifteen.map((item, index) => {
           return (
